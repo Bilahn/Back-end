897d39b2e841bf2946d61905cf8600cc17a35e86
baha-nouaili <b.noueili@gmail.com>
2021-07-03 08:11:51 +0100

    03-07-2021

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,43 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, UseGuards, HttpStatus, Req } from "@nestjs/common";
+import { AuthGuard } from "@nestjs/passport";
+import { Request } from "express";
+
+import { AppService } from "./app.service";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  // @Get('/google')
+  // @UseGuards(AuthGuard('google'))
+  // async googleAuth(@Req() req ) {
+  // }
+
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  sayHello(){
+    return 'Hello World!'
   }
+
+  // @Get('/auth/google/callback')
+  // @UseGuards(AuthGuard('google'))
+  // googleAuthRedirect(@Req() req) {
+  //   return this.appService.googleLogin(req)
+  // }
+
+
+  // @Get("/facebook")
+  // @UseGuards(AuthGuard("facebook"))
+  // async facebookLogin(): Promise<any> {
+    
+  //   return HttpStatus.OK;
+  // }
+
+  // @Get("/facebook/redirect")
+  // @UseGuards(AuthGuard("facebook"))
+  // async facebookLoginRedirect(@Req() req: Request): Promise<any> {
+  //   return {
+  //     statusCode: HttpStatus.OK,
+  //     data: req.user,
+  //   };
+  // }
 }
--- src/clients/clients.resolver.ts
+++ src/clients/clients.resolver.ts
@@ -7,36 +7,42 @@ import { LoginClientsInput } from './dto/login-clients.input';
 import { GqlAuthGuard } from '../guards/gql-auth.guards';
 import { UserToken } from './userToken.entity';
 import { CtxClient } from 'src/decoraters/ctx-user.decorator';
-import { PubSub } from 'graphql-subscriptions';
-
-const pubsub = new PubSub()
+import { ClientUpdateInput } from './dto/update-client'; 
 
 @Resolver(of => Clients)
 export class ClientsResolver {
     constructor(private clientService : ClientsService) {} 
  
-  @Mutation(()=>Clients)
-  register(@Args('createClientInput') createClientsInput : CreateClientsInput ) : Promise<Clients>  {
-      pubsub.publish('userAdded!' , { userAdded : createClientsInput })
+    @Mutation(()=>Clients)
+    register(@Args('createClientInput') createClientsInput : CreateClientsInput ) : Promise<Clients>  {
     console.log(createClientsInput)
         return this.clientService.register(createClientsInput) 
-  }
+    }
 
-  @Subscription(()=>Clients)
-  userAdded(){
-    return pubsub.asyncIterator('userAdded!')
-  }
+ 
 
-  @UseGuards(GqlAuthGuard)
+    @UseGuards(GqlAuthGuard)
     @Query(()=>Clients)
     getClientById(@CtxClient() client : Clients ){
       return this.clientService.findClientById(client.id) 
+    }
+
+    @UseGuards(GqlAuthGuard)
+    @Mutation(()=>String)
+    updateClient(@CtxClient() client : Clients , @Args('update') clientInput : ClientUpdateInput ){
+      console.log("clientId",client.id,"clientInput",clientInput)
+      return this.clientService.updateById(client.id,clientInput)
     }
 
    @Mutation(()=>UserToken)
    login(@Args('loginClientsInput') loginClientsInput : LoginClientsInput) : Promise<UserToken> {
      console.log(loginClientsInput)
            return this.clientService.login(loginClientsInput) ; 
+   }
+
+   @Query(() => [Clients], { name: 'allClients' })
+   findAll() {
+     return this.clientService.findAll();
    }
 
 }
